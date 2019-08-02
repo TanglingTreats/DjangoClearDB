489771c834ede3d18b2f510e27298788b42cbb29
Ubuntu <ubuntu@ip-172-31-29-198.ap-southeast-1.compute.internal>
2019-08-02 07:15:05 +0000

    change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,3 +147,7 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
