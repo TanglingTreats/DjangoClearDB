cf62df231173489daf58168eaa47940debdc0138
Ubuntu <ubuntu@ip-172-31-29-198.ap-southeast-1.compute.internal>
2019-08-02 07:29:56 +0000

    change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # extra lookup directories for collectstatic
