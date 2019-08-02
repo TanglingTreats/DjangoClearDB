c2dc191b2afe4e9fdcdbfe22c857fe8e206329aa
Ubuntu <ubuntu@ip-172-31-29-198.ap-southeast-1.compute.internal>
2019-08-02 07:55:58 +0000

    change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,7 +144,7 @@ STATICFILES_DIRS = (
 )
 
 # add configuration for static files storage using whitenoise
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
 
